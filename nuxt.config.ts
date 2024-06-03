ce6eaa38b3c1d2887c61c763a66669cc4bf22b6f
Francesco <francesco.loiacono@linksmt.it>
2024-06-03 08:39:43 +0200

    update nuxt config for supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/supabase"]
+  modules: ["@nuxt/ui", "@nuxtjs/supabase"],
+
+  runtimeConfig: {
+    public: {
+      supabase: {
+        signUp: {
+          emailRedirectTo: 'http://loacalhost:3000', // can be overridden by NUXT_PUBLIC_SUPABASE_SIGN_UP_EMAIL_REDIRECT_TO environment variable
+        }
+      }
+    }
+  },
+
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false,
+  }
 })
